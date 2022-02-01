50f4006b68fd79296a230f88737c8deeb1128c85
David Drummer <david.b.drummer@gmail.com>
2022-02-01 12:09:20 -0500

    commit tree e5d9d0e3a89515f99fbcf831080a470bb597cb07

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+// create function: distanceFromHqInBlocks
+
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+};
+
+// create function: distanceFromHqInFeet 
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+};
+
+// create function: distanceTravelledInFeet
+
+function distanceTravelledInFeet(start, stop) {
+    return Math.abs((start - stop) * 264);
+}
+
+// create function: calculatesFarePrice
+
+function calculatesFarePrice(start, stop) {
+    let fare;
+    let feet = distanceTravelledInFeet(start, stop);
+    if (feet <= 400) {
+        fare = 0;
+    } else if (feet > 400 && feet <= 2000) {
+        fare = (feet - 400) * .02;
+    } else if (feet > 2000 && feet <= 2500) {
+        fare =  25;
+    }else if (feet > 2500) {
+        fare = 'cannot travel that far';
+    };
+    return fare;
+};
